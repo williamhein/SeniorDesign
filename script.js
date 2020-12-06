9a48952f3e9e892841ee47a48eb8bf210ab28caf
Will Hein <willbillhein@gmail.com>
2020-12-06 10:30:58 -0600

    Adding back bar graph cause it is easier

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function updateData()
 }
 
 function addData(chart, label, data) {
-  chart.data.labels.push(label);
+  //chart.data.labels.push(label);
   chart.data.datasets.forEach((dataset) => {
       dataset.data.push(data);
   });
