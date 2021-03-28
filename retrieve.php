5ceefe6d1f922ea4371dfff376dfc40a99af3d3e
Will Hein <willbillhein@gmail.com>
2021-03-28 17:29:11 -0500

    Adding dynamic function to pull data

--- retrieve.php
+++ retrieve.php
@@ -1,5 +1,31 @@
 <?php 
 
+    $servername = "localhost";
+    $username = "root";
+    $password = "Br@mbl3"; //ignore the fact that this is plain text
+                            //when we make users, we need to make a read only users for explicity this code
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) 
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $conn->query("use garden_info");
+
+    $end_date = $_GET["et"];
+    $start_date = $_GET["st"];
+
+    
+    $tableInfo = [
+        "records_temp" => "temp",
+        "records_humidity" => "humidity",
+        "records_moisture" => "moisture",
+    ];
+
+
     function echoInfo($table)
     {
 
@@ -24,30 +50,6 @@
         } 
     }
     
-    $servername = "localhost";
-    $username = "root";
-    $password = "Br@mbl3"; //ignore the fact that this is plain text
-                            //when we make users, we need to make a read only users for explicity this code
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
-
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-    $conn->query("use garden_info");
-
-    $end_date = $_GET["et"];
-    $start_date = $_GET["st"];
-
-    
-    $tableInfo = [
-        "records_temp" => "temp",
-        "records_humidity" => "humidity",
-        "records_moisture" => "moisture",
-    ];
-
     if ($_GET["table"] == "all")
     {
         foreach ($tableInfo as $key => $value) 
