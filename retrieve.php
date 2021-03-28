9b824f47180f3194ef19ad26b330ce27965f5f8e
Will Hein <willbillhein@gmail.com>
2021-03-28 17:17:45 -0500

    Adding dynamic function to pull data

--- retrieve.php
+++ retrieve.php
@@ -1,6 +1,29 @@
 <?php 
 
-    /*
+    function echoInfo($table)
+    {
+
+        $sql = "SELECT * FROM " . $table ." WHERE time BETWEEN '" . $start_date . "' AND '" .  $end_date . "'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) 
+        {
+            // output data of each row
+            $max = strtotime("1900-1-1 12:00:00");
+            $max_key = "";
+            while($row = $result->fetch_assoc()) 
+            {
+                if (strtotime($row["time"])>$max)
+                {
+                    $max = strtotime($row["time"]);
+                    $max_key = $row[$tableInfo[table]]; 
+                }
+
+                echo $row["time"] ."!". $row[$tableInfo[table]] ."!". $row["row_id"] . ";"; 
+            }
+        } 
+    }
+    
     $servername = "localhost";
     $username = "root";
     $password = "Br@mbl3"; //ignore the fact that this is plain text
@@ -38,28 +61,5 @@
         echoInfo($_GET["table"]);
     }
 
-    function echoInfo($table)
-    {
-
-        $sql = "SELECT * FROM " . $table ." WHERE time BETWEEN '" . $start_date . "' AND '" .  $end_date . "'";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) 
-        {
-            // output data of each row
-            $max = strtotime("1900-1-1 12:00:00");
-            $max_key = "";
-            while($row = $result->fetch_assoc()) 
-            {
-                if (strtotime($row["time"])>$max)
-                {
-                    $max = strtotime($row["time"]);
-                    $max_key = $row[$tableInfo[table]]; 
-                }
-
-                echo $row["time"] ."!". $row[$tableInfo[table]] ."!". $row["row_id"] . ";"; 
-            }
-        } 
-    }
-    */
+    
 ?>
