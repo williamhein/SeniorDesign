2c48756c1f2130ce5a0bc2310c81b5de5b5de431
Will Hein <willbillhein@gmail.com>
2021-03-28 17:13:33 -0500

    Adding dynamic function to pull data

--- retrieve.php
+++ retrieve.php
@@ -1,62 +1,65 @@
-<?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "Br@mbl3"; //ignore the fact that this is plain text
-                                    //when we make users, we need to make a read only users for explicity this code
+<?php 
 
-            // Create connection
-            $conn = new mysqli($servername, $username, $password);
+    /*
+    $servername = "localhost";
+    $username = "root";
+    $password = "Br@mbl3"; //ignore the fact that this is plain text
+                            //when we make users, we need to make a read only users for explicity this code
 
-            // Check connection
-            if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            }
-            $conn->query("use garden_info");
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
 
-            $end_date = $_GET["et"];
-            $start_date = $_GET["st"];
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    $conn->query("use garden_info");
 
-            
-            $tableInfo = [
-                "records_temp" => "temp",
-                "records_humidity" => "humidity",
-                "records_moisture" => "moisture",
-            ];
+    $end_date = $_GET["et"];
+    $start_date = $_GET["st"];
 
-            if ($_GET["table"] == "all")
+    
+    $tableInfo = [
+        "records_temp" => "temp",
+        "records_humidity" => "humidity",
+        "records_moisture" => "moisture",
+    ];
+
+    if ($_GET["table"] == "all")
+    {
+        foreach ($tableInfo as $key => $value) 
+        {
+            echoInfo($key);
+            echo "&";
+        }
+    }
+    else
+    {
+        echoInfo($_GET["table"]);
+    }
+
+    function echoInfo($table)
+    {
+
+        $sql = "SELECT * FROM " . $table ." WHERE time BETWEEN '" . $start_date . "' AND '" .  $end_date . "'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) 
+        {
+            // output data of each row
+            $max = strtotime("1900-1-1 12:00:00");
+            $max_key = "";
+            while($row = $result->fetch_assoc()) 
             {
-                foreach ($tableInfo as $key => $value) 
+                if (strtotime($row["time"])>$max)
                 {
-                    echoInfo($key);
-                    echo "&";
+                    $max = strtotime($row["time"]);
+                    $max_key = $row[$tableInfo[table]]; 
                 }
+
+                echo $row["time"] ."!". $row[$tableInfo[table]] ."!". $row["row_id"] . ";"; 
             }
-            else
-            {
-                echoInfo($_GET["table"]);
-            }
-
-            function echoInfo($table)
-            {
-
-                $sql = "SELECT * FROM " . $table ." WHERE time BETWEEN '" . $start_date . "' AND '" .  $end_date . "'";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) 
-                {
-                    // output data of each row
-                    $max = strtotime("1900-1-1 12:00:00");
-                    $max_key = "";
-                    while($row = $result->fetch_assoc()) 
-                    {
-                        if (strtotime($row["time"])>$max)
-                        {
-                            $max = strtotime($row["time"]);
-                            $max_key = $row[$tableInfo[table]]; 
-                        }
-
-                        echo $row["time"] ."!". $row[$tableInfo[table]] ."!". $row["row_id"] . ";"; 
-                    }
-                } 
-            }
+        } 
+    }
+    */
 ?>
