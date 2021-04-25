ad6925e4b753dc112f90d9860ee12c6818ef6b2a
Will Hein <willbillhein@gmail.com>
2021-04-25 14:42:31 -0500

    Trying to get Maleea's pretty CSS back

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-   <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro.min.css">
-	<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-colors.min.css">
-	<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-rtl.min.css">
-	<link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-icons.min.css">
+  	<!-- <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro.min.css">-->
+	<!-- <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-colors.min.css">-->
+	<!-- <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-rtl.min.css">-->
+	<!-- <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-icons.min.css">-->
 
    <link rel="stylesheet" href="styles.css">
    <div class="container" id="blur">
