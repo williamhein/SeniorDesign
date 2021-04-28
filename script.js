830b7e8878354ec97593e29a4bfe22cad4aad166
Will Hein <willbillhein@gmail.com>
2021-04-27 20:34:00 -0500

    Making time work right on graphs.

--- script.js
+++ script.js
@@ -514,8 +514,7 @@ function graph5() {
               "rgba(239, 149, 107, 1)",
               "rgba(235, 107, 239, 1)"
           ],
-          borderWidth: 1,
-          fill: false
+          borderWidth: 1
       }]
   },
   options: {
@@ -549,39 +548,43 @@ function graph5() {
 function graph6() {
   var ctx6 = document.getElementById('myChart6');
   var temps6 = [65, 50, 62, 73, 52, 50, 71];
-  myChart6 = new Chart(ctx6, {
-  type: 'line',
-  data: {
-      labels: [...days],
+  myChart6 = new Chart(ctx6, config4 = {
+    type: 'line',
+    data: {
       datasets: [{
-          label: 'Plant Moisture (%)',
-          data: [],
-          backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(239, 149, 107, 0.2)",
-              "rgba(235, 107, 239, 0.2)"
-            ],
-          borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(239, 149, 107, 1)",
-              "rgba(235, 107, 239, 1)"
-          ],
-          borderWidth: 1
+        label: 'Plant Moisture (%)',
+        backgroundColor: "rgba(255, 99, 132, 0.6)",
+        borderColor: "rgba(255, 99, 132, 0.8)",
+        fill: false,
+        lineTension: 0.1,
+        data: [],
       }]
-  },
-  options: {
-    legend: {
-      onClick: null
+    },
+    options: {
+      responsive: true,
+      scales: {
+        xAxes: [{
+          type: 'time',
+          display: true,
+          ticks: {
+            fontSize: 17,
+            major: {
+              fontStyle: 'bold',
+              fontColor: '#FF0000',
+            }
+          }
+        }],
+        yAxes: [{
+          display: true,
+          ticks: {
+            fontSize: 17
+          }
+        }]
+      },
+      legend: {
+        onClick: null
+      }
     }
-  }
   })
 }
 
