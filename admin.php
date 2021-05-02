f8ccf672538c7e8d758ec9fbef8aa0883d7806be
Will Hein <willbillhein@gmail.com>
2021-05-01 20:33:28 -0500

    Working on sending time.

--- admin.php
+++ admin.php
@@ -18,7 +18,7 @@
             <!--<p style="font-size: 45px; color: white;">-->
             <p style="color: white;">
                 <!--<span style="font-weight: lighter; line-height:20px;">-->
-                    <span style="text-align: left; width: 70%">Start Time:</span>
+                    <div style="text-align: left; width: 70%">Start Time:</div>
                     <br>
                     <span style="font-weight: lighter; font-size: 140px;">
                     <input id = "r1hs" type="text" class="dial" data-min="1" data-max="12" data-height="110px" data-fgColor="#b2db94" value="12">
@@ -30,7 +30,7 @@
 					</select>
                     </span>
                 <br>
-                    <span style="text-align: left; width: 70%">End Time:</span>
+                    <div style="text-align: left; width: 70%">End Time:</div>
                     <br>
                     <span style="font-weight: lighter;font-size: 140px;">
                     <input id = "r1he" type="text" class="dial" data-min="1" data-max="12" data-height="110px" data-fgColor="#b2db94" value="12">
