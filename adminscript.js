31806051703b4cf429be00c5a949823c5ef31231
Will Hein <willbillhein@gmail.com>
2021-05-02 15:49:38 -0500

    Working on website security.

--- adminscript.js
+++ adminscript.js
@@ -17,7 +17,8 @@ function toggle1() {
     var blur1 = document.getElementById('blur1');
     blur1.classList.toggle('active');  //when the gear is clicked, the blur is initiated
     var popup1 = document.getElementById('popup1');
-    popup.classList.toggle('active');
+    popup1.classList.toggle('active');
+    
 }
 
 function getMySQLTime(h,m,ap) {
