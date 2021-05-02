bc2cf5ed943a247ed57ed1c138d8d2f90a6e5884
Will Hein <willbillhein@gmail.com>
2021-05-02 16:24:37 -0500

    Working on website security.

--- admin.php
+++ admin.php
@@ -112,6 +112,7 @@
                 $sql = $con->query("SELECT * FROM admin WHERE id = 1;");
                 $data = $sql->fetch_assoc();
                 if (password_verify($password, $data['pass'])) {
+                    echo '<script type="text/javascript">alert("password!");</script>';
                     setcookie("pass", $password, time() + (86400 * 1), "/");
                 } else {
                     echo '<script type="text/javascript">toggle1("Invalid password!");</script>';
